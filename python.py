36beaf552b3be97eb58afe8b4435529c64bf9541
Ubuntu <ubuntu@ip-172-31-19-56.eu-west-1.compute.internal>
2019-07-04 15:46:01 +0000

    Adding vending machine code

--- python.py
+++ python.py
@@ -1,8 +1,2 @@
-def number_of_evens(number):
-    return False
 
-def test_are_equal(actual, expected):
-    assert expected == actual, "Expected {0}, but got {1}".format(expected, actual)
-    
-test_are_equal(number_of_evens([1,2,3,4,5]), 2)
     
--- vending_machine.py
+++ vending_machine.py
@@ -1,48 +1,24 @@
 from byotest import *
 
-''' doc-string
-eur_coins = [100, 50, 20, 10, 5, 2, 1]
-usd_coins = [100, 50, 25, 10, 5, 1]
-'''
-
-#storing coin values in dictionary
-
-''' Working with dictionary of EU coins
-usd_coins = {
-    1: 20, 
-    5: 20, 
-    10: 20, 
-    25: 20, 
-    50 :20, 
-    100:20
-}
-
-'''
-
-eur_coins = { 
-    1: 20, 
-    2: 20, 
-    5: 20, 
-    10: 20, 
-    20: 20, 
-    50: 20, 
-    100: 20
-}
+usd_coins = {100:20, 50:20, 25:20, 10:20, 5:20, 1:20}
+eur_coins = {200:20, 100:20, 50:20, 20:20, 10:20, 5:20, 2:20, 1:20}
 
 def get_change(amount, coins=eur_coins):
+    
     change = []
-    for coin in coins:
-        while coin <= amount:
-            if coins[coin] == 0:
-
-            else:
-                amount -=coin
-                change.append(coin)
-            else:
-                return False
-            
-           
+    
+    for denomination in sorted(coins.keys(), reverse=True):
+        while denomination <= amount and coins[denomination] > 0:
+            amount -= denomination
+            coins[denomination] -= 1
+            change.append(denomination)
+    
+    if amount != 0:
+     raise Exception("Insufficient coins to give change!")
+     
     return change
+    
+    
 
 test_are_equal(get_change(0), [])
 test_are_equal(get_change(1), [1])
@@ -56,6 +32,7 @@ test_are_equal(get_change(3), [2,1])
 test_are_equal(get_change(7), [5,2])
 test_are_equal(get_change(9), [5,2,2])
 test_are_equal(get_change(35, usd_coins), [25,10])
+test_are_equal(get_change(5, {2:1, 1:4}), [2, 1, 1, 1])
 
 print("All tests pass!")
 
