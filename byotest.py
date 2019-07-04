6a20723794e6c01c2ee516516b4e992687514655
Ubuntu <ubuntu@ip-172-31-19-56.eu-west-1.compute.internal>
2019-07-04 11:09:34 +0000

    Changing file name and adding range test

--- byotest.py
+++ byotest.py
@@ -13,4 +13,9 @@ def test_not_in_collection(collection, item):
 def test_between_numbers(range, number):
     assert number in range, "{0} does not contain {1}".format(range, number)
     
-    
+def test_between(upper_limit, lower_limit, actual):
+    """
+    Check to ensure that a number is between two other numbers. Raises
+    AssertionError if the number is not between the other two numbers
+    """
+    assert lower_limit <= actual <= upper_limit, "{0} is not between {1} and {2}".format(actual, lower_limit, upper_limit)
